7e4bef7b813c3c542e6f7563a4cc07ed96bc0428
atish-ram-chandra <54966485+atish-ram-chandra@users.noreply.github.com>
2019-09-06 13:29:52 +1000

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Build.Locator;
+
+namespace Nunit3DiscoveryFailure
+{
+	public sealed class Initialization
+	{
+		public static void EnsureInitialized()
+		{
+			if (MSBuildLocator.IsRegistered)
+			{
+				return;
+			}
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace Nunit3DiscoveryFailure
+{
+	public class Program
+	{
+		[TestCaseSource(nameof(AllTypes))]
+		public void Test(Type type) => Assert.That(type, Is.Not.Null);
+
+
+		public static IEnumerable<Type> AllTypes
+		{
+			get
+			{
+				Initialization.EnsureInitialized();
+
+				yield return typeof(Program);
+			}
+		}
+	}
+}
